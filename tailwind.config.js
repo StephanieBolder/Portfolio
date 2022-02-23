6940481c413e96103ea294d49e3d1f9814b47e89
Stephanie Bolder <stephanie@Stephanies-MacBook-Pro.local>
2022-02-23 12:38:00 +0100

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+      colors: {
+        'textdark': '#2C2C2C',
+        'textmedium': '#928F8B',
+        'text-ight': '#F9F9F9',
+        'backgroundbeige': '#EEEAE1',
+        'backgroundblack': '#2C2C2C',
+        'lilac': '#E8CFFC',
+      },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+        yipes: ['yipes', 'sans-serif'],
+        poppins: ['poppins', 'sans-serif'],
+      },
+    }
+  },
+}
